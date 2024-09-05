b247013556439ca9fa6549a2d2e1b88494551754
Lucas Pontes <89760771+Lucvsd@users.noreply.github.com>
2024-09-05 15:09:16 -0300

    Add files via upload

--- Imersao_AluraGemini/dados.js
+++ Imersao_AluraGemini/dados.js
@@ -1,1 +1,19 @@
-let dados = "Raysa Leal"
+let dados = ["Raysa Leal", "Rebeca Adreade", "Breatriz Sousa"]
+
+let rayssa = {
+    titulo : "Rayssa Leal",
+    descricao :" Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens",
+    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"    
+}
+
+let rebeca = {
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é uma ginasta artística brasileira, campeã olímpica e mundial. Ela fez história ao conquistar a primeira medalha de ouro do Brasil na ginástica feminina, além de diversas outras conquistas importantes.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+};
+
+let beatriz = {
+    titulo: "Beatriz Sousa",
+    descricao: "Beatriz Sousa é uma atleta brasileira de natação, especializada no estilo peito. Ela já representou o Brasil em competições internacionais e continua sendo uma das promessas do esporte aquático no país.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Sousa"
+};
